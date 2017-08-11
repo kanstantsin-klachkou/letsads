1d369105041de43fa3d681d05885dbe0721c21da
Kanstantsin Klachkou <kanstantsin.klachkou@gmail.com>
2017-08-11 09:12:33 +0000

    Add Letsads::Client with #send_sms

---
+++ lib/letsads.rb
@@ -0,0 +1,7 @@
+require 'letsads/version'
+require 'letsads/client'
+require 'letsads/sms_sender'
+
+module Letsads
+  EXTERNAL_API_ENDPOINT = 'https://letsads.com/api'.freeze
+end
---
+++ lib/letsads/client.rb
@@ -0,0 +1,11 @@
+class Letsads::Client
+  def send_sms phone_numbers, message
+    sms_sender.send_sms phone_numbers, message
+  end
+
+  private
+
+  def sms_sender
+    @sms_sender ||= Letsads::SmsSender.new
+  end
+end
