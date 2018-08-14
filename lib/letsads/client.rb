ac58db09a5d9d4c6e6c75cd7583e4056f881f36d
Kanstantsin Klachkou <kanstantsin.klachkou@gmail.com>
2018-08-14 14:15:12 +0300

    Add balance checking

--- lib/letsads/client.rb
+++ lib/letsads/client.rb
@@ -3,6 +3,14 @@ class Letsads::Client
     sms_sender.send_sms(phone_numbers, message)
   end
 
+  def balance
+    xml_builder = Letsads::XMLBuilder.new
+    xml         = xml_builder.balance_xml
+    end_point   = Letsads::EXTERNAL_API_ENDPOINT
+
+    RestClient.post(end_point, xml, content_type: :xml)
+  end
+
   private
 
   def sms_sender
